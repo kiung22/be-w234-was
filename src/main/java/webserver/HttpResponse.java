895f797be51882e8996f9e00d1cec907b2519e57
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-14 10:16:57 +0900

    FEAT: HttpResponse class 추가 HttpResponse 정보를 가지는 class 추가했습니다.

---
+++ src/main/java/webserver/HttpResponse.java
@@ -0,0 +1,22 @@
+package webserver;
+
+public class HttpResponse {
+
+    private int statusCode;
+    private byte[] header;
+    private byte[] body;
+
+    public HttpResponse(byte[] body, int statusCode) {
+        this.statusCode = statusCode;
+        this.body = body;
+        this.header = String.format("HTTP/1.1 %d OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: %d\r\n\r\n", statusCode, body.length).getBytes();
+    }
+
+    public byte[] getHeader() {
+        return header;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+}
