6626bff293c42f43567b549a4062bf680f6b3a9b
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-14 17:39:48 +0900

    TEST: Controller 테스트 코드 추가

--- src/test/java/webserver/ControllerTest.java
+++ src/test/java/webserver/ControllerTest.java
@@ -17,7 +17,7 @@ class ControllerTest {
 
     @Test
     @DisplayName("index 요청에 대해 index.html를 응답합니다.")
-    void indexRequest() throws IOException {
+    void indexRequest() throws Exception {
         InputStream inputStream = new ByteArrayInputStream("GET /index.html HTTP/1.1\nHost: localhost:8080\nConnection: keep-alive\nAccept: */*".getBytes());
         HttpRequest httpRequest = new HttpRequest(inputStream);
         HttpResponse httpResponse = controller.requestMapping(httpRequest);
@@ -27,8 +27,8 @@ class ControllerTest {
     }
 
     @Test
-    @DisplayName("잘못된 요청에 대해 404.html를 응답합니다.")
-    void badRequest() throws IOException {
+    @DisplayName("path가 잘못된 요청에 대해 404.html를 응답합니다.")
+    void NotFoundRequest() throws Exception {
         InputStream inputStream = new ByteArrayInputStream("GET /bad-request HTTP/1.1\nHost: localhost:8080\nConnection: keep-alive\nAccept: */*".getBytes());
         HttpRequest httpRequest = new HttpRequest(inputStream);
         HttpResponse httpResponse = controller.requestMapping(httpRequest);
