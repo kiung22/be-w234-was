85eee055a7f36275424d37f71367602aa0882688
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-14 17:40:11 +0900

    TEST: HttpRequest 테스트 코드 추가

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -20,4 +20,17 @@ class HttpRequestTest {
         assertThat(httpRequest.getMethod()).isEqualTo(HttpMethod.GET);
         assertThat(httpRequest.getPath()).isEqualTo("/index.html");
     }
+
+    @Test
+    @DisplayName("회원가입이 잘 됩니다.")
+    void signup() {
+        InputStream inputStream = new ByteArrayInputStream("GET /user/create?userId=test&password=password&name=test&email=test%40slipp.net HTTP/1.1\nHost: localhost:8080\nConnection: keep-alive\nAccept: */*".getBytes());
+        HttpRequest httpRequest = new HttpRequest(inputStream);
+
+        assertThat(httpRequest.getMethod()).isEqualTo(HttpMethod.GET);
+        assertThat(httpRequest.getQuery().get("userId")).isEqualTo("test");
+        assertThat(httpRequest.getQuery().get("password")).isEqualTo("password");
+        assertThat(httpRequest.getQuery().get("name")).isEqualTo("test");
+        assertThat(httpRequest.getQuery().get("email")).isEqualTo("test@slipp.net");
+    }
 }
