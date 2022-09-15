532070c37755d9f15d42ff0d7ae3e2721b788a99
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-15 15:28:36 +0900

    FEAT: HttpStatus enum class 추가 status code와 status message를 필드로 가지는 enum class를 추가했습니다.

---
+++ src/main/java/http/HttpStatus.java
@@ -0,0 +1,31 @@
+package http;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int statusCode;
+    private final String statusMessage;
+
+    HttpStatus(int statusCode, String statusMessage) {
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public static HttpStatus valueOfStatusCode(int statusCode) {
+        for (HttpStatus status : HttpStatus.values()) {
+            if (status.getStatusCode() == statusCode) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 status code입니다.");
+    }
+}
