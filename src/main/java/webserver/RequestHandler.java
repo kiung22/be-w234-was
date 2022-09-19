65933c3995b04e11c4ea5ca2bdceacfa1000f99c
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-19 19:56:14 +0900

    readRequest 메소드 생성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,23 +1,21 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Map;
 
+import http.HttpMethod;
+import http.HttpRequestParser;
 import http.HttpResponse;
 import http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import util.HttpResponseUtils;
-
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     private final Controller controller = new Controller();
 
@@ -30,23 +28,42 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            HttpRequest httpRequest = new HttpRequest(in);
+            HttpRequest httpRequest = readRequest(in);
             DataOutputStream dos = new DataOutputStream(out);
             HttpResponse httpResponse = controller.requestMapping(httpRequest);
             writeResponse(dos, httpResponse);
-        } catch (Exception e) {
+        } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void writeResponse(DataOutputStream dos, HttpResponse httpResponse) {
-        try {
-            dos.write(HttpResponseUtils.statusLineToByte(httpResponse.getVersion(), httpResponse.getStatus()));
-            dos.write(HttpResponseUtils.headerToByte(httpResponse.getHeader()));
-            dos.write(HttpResponseUtils.fileToByte(httpResponse.getBody()));
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    private HttpRequest readRequest(InputStream inputStream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String line = br.readLine();
+        Map<String, String> statusLineMap = HttpRequestParser.parseStatusLine(line);
+        HttpMethod method = HttpMethod.valueOf(statusLineMap.get("method"));
+        String path = statusLineMap.get("path");
+        String queryString = statusLineMap.get("queryString");
+        Map<String, String> query = HttpRequestParser.parseQueryString(queryString);
+
+        logger.info("Http Method: {}", method);
+        logger.info("Http Path: {}", path);
+        logger.info("Http Query: {}", query);
+        while (line != null && !line.isEmpty()) {
+            logger.debug(line);
+            line = br.readLine();
         }
+        return new HttpRequest(
+                method,
+                path,
+                query
+        );
+    }
+
+    private void writeResponse(DataOutputStream dos, HttpResponse httpResponse) throws IOException {
+        dos.write(httpResponse.getStatusLineToBytes());
+        dos.write(httpResponse.getHeaderToBytes());
+        dos.write(httpResponse.getBodyToBytes());
+        dos.flush();
     }
 }
