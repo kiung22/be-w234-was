d4633f12d531c1a84df4c9f9e622facb6c55b757
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-21 12:56:13 +0900

    Cookie class 추가

---
+++ src/main/java/http/Cookie.java
@@ -0,0 +1,31 @@
+package http;
+
+public class Cookie {
+
+    private final String name;
+    private final String value;
+    private String path;
+
+    public Cookie(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Cookie setPath(String path) {
+        this.path = path;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        String cookieString = name + "=" + value;
+        if (path != null) {
+            cookieString = cookieString + "; path=" + path;
+        }
+        return cookieString;
+    };
+}
