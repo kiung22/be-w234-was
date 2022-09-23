886c850a59e917b8d1e1e26b06c5d13b43d8f9d9
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-23 15:39:35 +0900

    getMapper 추가

--- src/main/java/webserver/Controller.java
+++ src/main/java/webserver/Controller.java
@@ -7,22 +7,29 @@ import service.LoginService;
 import service.Service;
 import service.SignupService;
 import service.StaticFileService;
+import service.UserListService;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Controller {
 
+    private final Map<String, Service> getMapper = new HashMap<>();
     private final Map<String, Service> postMapper = new HashMap<>();
     private final StaticFileService staticFileService = new StaticFileService();
 
     public Controller() {
+        getMapper.put("/user/list.html", new UserListService());
+
         postMapper.put("/user/create", new SignupService());
         postMapper.put("/user/login", new LoginService());
     }
 
     public HttpResponse requestMapping(HttpRequest httpRequest) {
         if (httpRequest.getMethod() == HttpMethod.GET) {
+            if (getMapper.containsKey(httpRequest.getPath())) {
+                return getMapper.get(httpRequest.getPath()).run(httpRequest);
+            }
             return staticFileService.run(httpRequest);
         } else if (httpRequest.getMethod() == HttpMethod.POST) {
             return postMapper.get(httpRequest.getPath()).run(httpRequest);
