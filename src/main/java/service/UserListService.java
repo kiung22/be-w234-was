3cc4f51c71fe3403f3cfc97f780cb22c008063b8
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-23 15:44:34 +0900

    유저 목록 조회하는 기능 추가

---
+++ src/main/java/service/UserListService.java
@@ -0,0 +1,55 @@
+package service;
+
+import db.Database;
+import http.HttpRequest;
+import http.HttpResponse;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class UserListService implements Service {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    @Override
+    public HttpResponse run(HttpRequest httpRequest) {
+        try {
+            String html = insertUserList();
+            return new HttpResponse(200)
+                .setBody(html.getBytes());
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            return new HttpResponse(500)
+                    .setBody(new File("./webapp/500.html"));
+        }
+    }
+
+    private String insertUserList() throws IOException {
+        String html = Files.readString(Path.of("./webapp/user/list.html"));
+        StringBuilder stringBuilder = new StringBuilder(html);
+        return stringBuilder.insert(stringBuilder.lastIndexOf("</tbody>"), getUserListToString()).toString();
+    }
+
+    private String getUserListToString() {
+        Collection<User> userList = Database.findAll();
+        AtomicInteger i = new AtomicInteger(2);
+        return userList.stream()
+                .map(user -> String.format(
+                        "<tr><th scope=\"row\">%d</th> <td>%s</td> <td>%s</td> <td>%s</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td></tr>",
+                        i.incrementAndGet(),
+                        user.getUserId(),
+                        user.getName(),
+                        user.getEmail()
+                ))
+                .collect(Collectors.joining("\n"));
+    }
+}
