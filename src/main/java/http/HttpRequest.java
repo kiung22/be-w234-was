c57fae9147409c4e789c52bd796d099babc318f2
kakao <jake@kakaoui-MacBookPro.local>
2022-09-27 15:44:19 +0900

    HttpRequest body 필드 타입 변경 및 Builder pattern 적용

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,7 +1,6 @@
 package http;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -11,29 +10,16 @@ public class HttpRequest {
     private final String path;
     private final Map<String, String> query;
     private final Map<String, String> header;
-    private final Map<String, String> body;
+    private final String body;
     private final Map<String, Cookie> cookie;
 
-    public HttpRequest(
-            HttpMethod method,
-            String path,
-            Map<String, String> query,
-            Map<String, String> header,
-            Map<String, String> body
-    ) {
-        this.method = method;
-        this.path = path;
-        this.query = query;
-        this.header = header;
-        this.body = body;
-        if (this.header.containsKey("Cookie")) {
-            Map<String, String> cookieStringMap = HttpRequestParser.parseCookies(this.header.get("Cookie"));
-            cookie = cookieStringMap.entrySet().stream()
-                    .collect(Collectors.toMap(e -> e.getKey(), e -> new Cookie(e.getKey(), e.getValue())));
-            System.out.println(cookie);
-        } else {
-            cookie = new HashMap<>();
-        }
+    private HttpRequest(Builder builder) {
+        method = builder.method;
+        path = builder.path;
+        query = builder.query;
+        header = builder.header;
+        body = builder.body;
+        cookie = builder.cookie;
     }
 
     public HttpMethod getMethod() {
@@ -52,11 +38,56 @@ public class HttpRequest {
         return Collections.unmodifiableMap(header);
     }
 
-    public Map<String, String> getBody() {
-        return Collections.unmodifiableMap(body);
+    public String getBody() {
+        return body;
+    }
+
+    public Map<String, String> getBodyToMap() {
+        if (!"application/x-www-form-urlencoded".equals(header.get("Content-Type"))) {
+            throw new RuntimeException("http request의 content type이 form-urlencoded이 아닙니다.");
+        }
+        return HttpRequestParser.parseBodyString(body);
     }
 
     public Map<String, Cookie> getCookie() {
         return Collections.unmodifiableMap(cookie);
+    }
+
+    public static class Builder {
+        private final HttpMethod method;
+        private final String path;
+        private Map<String, String> query;
+        private Map<String, String> header;
+        private String body;
+        private Map<String, Cookie> cookie;
+
+        public Builder(HttpMethod method, String path) {
+            this.method = method;
+            this.path = path;
+        }
+
+        public Builder setQuery(Map<String, String> query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder setHeader(Map<String, String> header) {
+            this.header = header;
+            if (header.containsKey("Cookie")) {
+                Map<String, String> cookieStringMap = HttpRequestParser.parseCookies(this.header.get("Cookie"));
+                cookie = cookieStringMap.entrySet().stream()
+                        .collect(Collectors.toMap(e -> e.getKey(), e -> new Cookie(e.getKey(), e.getValue())));
+            }
+            return this;
+        }
+
+        public Builder setBody(String body) {
+            this.body = body;
+            return this;
+        }
+
+        public HttpRequest build() {
+            return new HttpRequest(this);
+        }
     }
 }
