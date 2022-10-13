a5310af393c8d7e05adcb020d769872499454d05
kiung22 <rldnd22@gmail.com>
2022-10-13 10:28:25 +0900

    logger 제거

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -2,18 +2,10 @@ package controller;
 
 import http.HttpRequest;
 import http.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import service.MemoService;
-import webserver.RequestHandler;
-
-import java.io.File;
-import java.io.IOException;
-
 
 public class IndexController implements Controller {
 
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final MemoService memoService;
 
     public IndexController(MemoService memoService) {
